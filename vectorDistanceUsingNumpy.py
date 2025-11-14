f8883825d3c5696a7306bc0370718346877e8681
rajee99 <rajee0585@gmail.com>
2025-11-14 16:31:28 +0600

    added comments

--- vectorDistanceUsingNumpy.py
+++ vectorDistanceUsingNumpy.py
@@ -3,7 +3,7 @@ import numpy as np
 u = np.array([3, 2])
 
 
-magnitude = np.linalg.norm(u)
-
+magnitude = np.linalg.norm(u) #In AI, physics, and math, “norm” = “how big is this vector?”
+ #  linalg = Short for Linear Algebra.
 print("u :", u)
 print("magnitude :", magnitude)
