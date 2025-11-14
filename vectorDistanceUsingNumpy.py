90c55718f4dfb08140c587882619a0fa95e15ea1
rajee99 <rajee0585@gmail.com>
2025-11-14 16:55:23 +0600

    day 5

--- vectorDistanceUsingNumpy.py
+++ vectorDistanceUsingNumpy.py
@@ -5,5 +5,7 @@ u = np.array([3, 2])
 
 magnitude = np.linalg.norm(u) #In AI, physics, and math, “norm” = “how big is this vector?”
  #  linalg = Short for Linear Algebra.
+unit_u = u / magnitude
 print("u :", u)
 print("magnitude :", magnitude)
+print(unit_u)
---
+++ vector_sub.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+Home = np.array([1, 5])
+spot = np.array([4, 1])
+
+difference = Home - spot
+
+print(difference)
